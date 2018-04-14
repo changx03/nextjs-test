28727f03707fb4c8c3c76140d55094a62875359a
Luke Chang <luke@csel.co.nz>
2018-04-14 15:40:11 +1200

    add a button

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Link from 'next/link';
+import Router from 'next/router';
 
 const indexPage = () => (
   <div>
     <h1>The main page</h1>
-    <Link href="/auth"><a>Auth</a></Link>
+    <Link href="/auth">Auth</Link>
+    <button onClick={() => { Router.push('/auth'); }}>Go to auth</button>
   </div>
 );
 
